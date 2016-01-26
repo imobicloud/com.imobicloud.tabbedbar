8b0182bb4802a08753d4fe7f6f4450afb5717c36
ptquang86 <ptquang86@gmail.com>
2016-01-26 11:12:34 +0700

    refactor

--- README.md
+++ README.md
@@ -9,20 +9,24 @@ xml
 
 app.tss
 
-    ".imc-tabbedbar[platform=ios]": { backgroundColor: '#fff' }
-    ".imc-tabbedbar[platform=android]": { width: Ti.UI.SIZE, height: 30, backgroundColor: '#fff', borderColor: '#fff', borderRadius: 3, borderWidth: 1, layout: 'horizontal' }
-        ".imc-tabbedbar-tab[platform=android]": { left: 1 }
-        ".imc-tabbedbar-tab-first[platform=android]": { left: 0 }
-        ".imc-tabbedbar-tab-off[platform=android]": { backgroundColor: '#000' }
-        ".imc-tabbedbar-tab-on[platform=android]": { backgroundColor: 'transparent' }
-            ".imc-tabbedbar-icon[platform=android]": {}
-            ".imc-tabbedbar-title[platform=android]": { width: Ti.UI.SIZE, left: 12, right: 12, font: { fontSize: 14 } }
-            ".imc-tabbedbar-title-off[platform=android]": { color: '#fff' }
-            ".imc-tabbedbar-title-on[platform=android]": { color: '#000' }
+	".tabbedbar": { height: 30 }
+	    ".imc-tabbedbar[platform=ios]": { height: '100%', backgroundColor: '#fff', style: Titanium.UI.iPhone.SystemButtonStyle.BAR }
+		".imc-tabbedbar[platform=android]": { width: Ti.UI.SIZE, backgroundColor: '#fff', borderColor: '#fff', borderRadius: 3, borderWidth: 1, layout: 'horizontal' }
+			".imc-tabbedbar-tab[platform=android]": { left: 1 }
+			".imc-tabbedbar-tab-first[platform=android]": { left: 0 }
+			".imc-tabbedbar-tab-off[platform=android]": { backgroundColor: '#000' }
+			".imc-tabbedbar-tab-on[platform=android]": { backgroundColor: 'transparent' }
+				".imc-tabbedbar-icon[platform=android]": {}
+				".imc-tabbedbar-title[platform=android]": { width: Ti.UI.SIZE, left: 12, right: 12, font: { fontSize: 14 } }
+				".imc-tabbedbar-title-off[platform=android]": { color: '#fff' }
+				".imc-tabbedbar-title-on[platform=android]": { color: '#000' }
+				".imc-tabbedbar-divider": { width: 1, left: 0, backgroundColor: '#fff' }
 
 js 
 
-    $.tabbedbar.load({
+    $.tabbedbar.load($, {
+    	useCustom: false, // use custom tabbedbar for ios
+    	useDivider: false, // use custom divider when useCustom is true
         index: 0,
         labels: [
             { title: 'Browse' }, // { enabled: true, image: '', title: '', width: 100 }
@@ -32,3 +36,5 @@ js
     });
 
 	$.tabbedbar.getIndex();
+	
+	$.tabbedbar.unload();
--- controllers/widget.js
+++ controllers/widget.js
@@ -1,74 +1,98 @@
-init(arguments[0]);
+var G, tabbedbar, useCustom;
+
+init(arguments[0] || {});
 function init(args) {
 	var exclude = ['id', 'children'];
-    $.tabbedbar.applyProperties( _.omit(args, exclude) );
+	$.container.applyProperties(_.omit(args, exclude));
 }
 
 /*
  params = {
+ 	useCustom: false,
  	index: 0,
  	labels: [{ enabled: true, image: '', title: '', width: 100 }]
  }
  * */
-exports.load = function(params) {
+exports.load = function(_G, params) {
+	G = _G;
+	useCustom = params.useCustom;
+	
 	var labels = params.labels,
 		index = params.index || 0;
-
-	if (OS_ANDROID) {
+	
+	if (OS_IOS && useCustom !== true) {
+		tabbedbar = G.UI.create('TabbedBar', { classes: 'imc-tabbedbar', labels: labels, index: index });
+	} else {
+		tabbedbar = G.UI.create('View', { classes: 'imc-tabbedbar' });
+		
 		for(var i=0,ii=labels.length; i<ii; i++){
 			var label = labels[i];
-			var icon, styleTab, styleTitle;
+			var icon, status;
 			if (i !== index) {
 				icon = label.image;
-				styleTab = 'imc-tabbedbar-tab imc-tabbedbar-tab-off';
-				styleTitle = 'imc-tabbedbar-title imc-tabbedbar-title-off';
+				status = '-off';
 			} else {
 				icon = label.imageOn || label.image;
-				styleTab = 'imc-tabbedbar-tab imc-tabbedbar-tab-on';
-				styleTitle = 'imc-tabbedbar-title imc-tabbedbar-title-on';
+				status = '-on';
 			}
-			if (i === 0) { styleTab += ' imc-tabbedbar-tab-first'; }
-			var tab = $.UI.create('View', { tabIndex: i, classes: styleTab, width: label.width || Ti.UI.SIZE, touchEnabled: label.enabled || true });
-				label.image && tab.add( $.UI.create('ImageView', { image: icon, classes: 'imc-tabbedbar-icon', touchEnabled: false }) );
-				label.title && tab.add( $.UI.create('Label', { text: label.title, classes: styleTitle, touchEnabled: false }) );
-		  	$.tabbedbar.add(tab);
+			var tab = G.UI.create('View', { tabIndex: i, classes: 'imc-tabbedbar-tab imc-tabbedbar-tab' + status });
+				if (i) {
+					params.useDivider && tab.add( G.UI.create('View', { classes: 'imc-tabbedbar-divider', touchEnabled: false }) );
+				} else {
+					tab.applyProperties( G.createStyle({ classes: 'imc-tabbedbar-tab-first' }) );
+				}
+				label.width != null && (tab.width = label.width);
+				label.enabled != null && (tab.touchEnabled = label.enabled);
+				label.image && tab.add( G.UI.create('ImageView', { image: icon, classes: 'imc-tabbedbar-icon', touchEnabled: false }) );
+				label.title && tab.add( G.UI.create('Label', { text: label.title, classes: 'imc-tabbedbar-title imc-tabbedbar-title' + status, touchEnabled: false }) );
+		  	tabbedbar.add(tab);
 		};
+		
+		tabbedbar.labels = labels;
+		tabbedbar.index = index;
 	}
 	
-	$.tabbedbar.labels = labels;
-	$.tabbedbar.index = index;
+	tabbedbar.addEventListener('click', tabbedbarClick);
+	
+	// $.addTopLevelView(tabbedbar);
+	$.container.add(tabbedbar);
 };
 
 exports.unload = function() {
-	$.tabbedbar.removeAllChildren();
+	tabbedbar.labels = [];
+	if (OS_ANDROID || useCustom) {
+		tabbedbar.removeAllChildren();
+	}
+	tabbedbar = null;
+	G = null;
 };
 
 function tabbedbarClick(e) {
 	var index;
-	if (OS_IOS) {
+	if (OS_IOS && useCustom !== true) {
 		index = e.index;
 	} else {
 		index = e.source.tabIndex;
-		var lastIndex = $.tabbedbar.index;
+		var lastIndex = tabbedbar.index;
 		if (index == null || index == lastIndex) { return; }
 		
-		var labels = $.tabbedbar.labels;
+		var labels = tabbedbar.labels;
 		
 		var tab = e.source;
-  		$.addClass(tab, 'imc-tabbedbar-tab-on');
-  		$.addClass(tab.children[1], 'imc-tabbedbar-title-on');
+		tab.applyProperties( G.createStyle({ classes: 'imc-tabbedbar-tab-on' }) );
+		tab.children[ labels[index].image ? 1 : 0 ].applyProperties( G.createStyle({ classes: 'imc-tabbedbar-title-on' }) );
   		if (labels[index].imageOn) { tab.children[0].image = labels[index].imageOn; }
   		
-  		var lastTab = $.tabbedbar.children[lastIndex];
-		$.addClass(lastTab, 'imc-tabbedbar-tab-off');
-		$.addClass(lastTab.children[1], 'imc-tabbedbar-title-off');
+  		var lastTab = tabbedbar.children[lastIndex];
+		lastTab.applyProperties( G.createStyle({ classes: 'imc-tabbedbar-tab-off' }) );
+		lastTab.children[ labels[lastIndex].image ? 1 : 0 ].applyProperties( G.createStyle({ classes: 'imc-tabbedbar-title-off' }) );
 		if (labels[lastIndex].imageOn) { lastTab.children[0].image = labels[lastIndex].image; }
 		
-		$.tabbedbar.index = index;
+		tabbedbar.index = index;
 	}
-	$.trigger('click', { index: index, label: $.tabbedbar.labels[index] });
+	$.trigger('click', { index: index, label: tabbedbar.labels[index] });
 }
 
 exports.getIndex = function() {
-	return $.tabbedbar.index;
+	return tabbedbar.index;
 };
